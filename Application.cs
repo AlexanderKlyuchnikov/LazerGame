bf2b0f41acf91d9c877bbfcfeda240cf6056ecf3
AlexanderKlyuchnikov <AlexanderK1707@yandex.ru>
2023-03-17 13:35:55 +0300

    Start development

---
+++ Application.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using System;
+using System.Windows;
+
+namespace LazerGame
+{
+	public partial class StartApplication : Application
+	{
+		public StartApplication()
+		{
+			//System.Console.WriteLine("LazerGame has started");
+			//System.Windows.Application.Current.Shutdown();
+		}
+	}
+}
